9a4af9e2a0d3cb38d542ac02e328d3e34fc71fea
maud08 <maudinette08@outlook.com>
2021-02-09 18:10:32 +0100

    add son "coup" and clearTimeout

--- src/index.js
+++ src/index.js
@@ -2,40 +2,45 @@ const scoreBoard = document.getElementById("score");
 const holes = document.getElementsByClassName("hole");
 const contentHoles = document.getElementById("container-a-mole");
 const buttonStart = document.getElementById("btn-start");
+const buttonStop = document.getElementsByClassName("btn-stop");
 const liveBoard = document.getElementById("live");
 const levelBoard = document.getElementById("level");
 const jokerLose = new Audio("src/son/joker-lose.mp3");
+const jokerTouch = new Audio ("src/son/coup.mp3");
 let levelUp = false;
 let lastHoles;
 let score = 0;
 let live = 5;
 let level = 1;
 let time;
+let holeActive;
+let changeMole;
 
 buttonStart.addEventListener("click", startGame);
 contentHoles.addEventListener("click", wack, false);
 
+
+
 /* La fonction Math.floor(x) renvoie le plus grand entier qui est inférieur ou égal à un nombre  */
 function randomHole(holes) {
   let index = Math.floor(Math.random() * holes.length);
-  const hole = holes[index];
-  if (hole === lastHoles) {
-    return randomHole(holes);
-  }
-  lastHoles = holes;
-
-  return hole;
+    const hole = holes[index];
+    if (hole === lastHoles) {
+        return randomHole(holes);
+    }
+    lastHoles = holes;
+    return hole;
 }
 
 function showTheMole() {
-  const holeActive = randomHole(holes);
+  holeActive = randomHole(holes);
   holeActive.classList.add("active-a-mole");
   if (live <= 0) {
     scoreBoard.textContent = 0;
     liveBoard.textContent = 0;
     return;
   }
-  setTimeout(() => {
+  changeMole = setTimeout(() => {
     holeActive.classList.remove("active-a-mole");
     if (levelUp === true) {
       time = time / 2;
@@ -52,6 +57,16 @@ function startGame() {
   scoreBoard.textContent = score;
   liveBoard.textContent = live;
   levelBoard.textContent = level;
+  buttonStart.textContent = "Stop";
+  buttonStart.classList.add("btn-stop");
+  buttonStart.removeEventListener("click", startGame);
+  // addEventListener se met sur un élement si j'ai un calsse je dois pécifier a quel niveau je veux le placer ou bouclé sur toutes les classe
+  buttonStop[0].addEventListener("click", stopGame);
+}
+
+function stopGame(){
+  // permet de supprimer le timeout et supprimer l'ajout de la classe active-a-mole
+  clearTimeout(changeMole);
 }
 
 function wack(e) {
@@ -66,7 +81,8 @@ function wack(e) {
     levelUp = true;
   } else {
     score++;
-    this.parentNode.classList.remove("active-a-mole");
+    jokerTouch.play();
+    e.target.parentNode.classList.remove("active-a-mole");
     scoreBoard.textContent = score;
   }
 }
